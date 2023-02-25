7111c5c54b6acb804bc2698ae54be81ee9babfc6
Suluoya <1931960436@qq.com>
2023-02-25 14:40:44 +0800

    New Feature (HidePrint)

--- wsy/log/log.py
+++ wsy/log/log.py
@@ -2,43 +2,70 @@
 # @Author: 昵称有六个字
 # @Date:   2022-10-20 11:34:28
 # @Last Modified by:   昵称有六个字
-# @Last Modified time: 2023-02-25 14:20:34
+# @Last Modified time: 2023-02-25 14:40:34
+import os
+import sys
 from random import choice
+from typing import TextIO
 
 import pretty_errors
-from termcolor import colored, cprint
+from icecream import ic
+from termcolor import cprint
 
 
 def wprint(content, color=None) -> None:
-    """Colorful output
-    Args:
-        content (_type_): Anything you want.
-        color (str): Choose from ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan'].
-        Nothing passed in → random color.
     """
-    color_list: list[str] = ["grey", "red", "green", "yellow", "blue", "magenta", "cyan"]
-    if color not in color_list:
-        cprint(text=content, color=choice(color_list), attrs=["bold"])
-    else:
-        cprint(text=content, color=color, attrs=["bold"])
+    Print colorize text.
+    --------
+    Args:
+    --------
+        content (_type_): Anything you want to print.
+        color (str): Choose from ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan'].
+                            Nothing passed in → Random Color.
+    Usages:
+    --------
+        >>> wprint("Anything you want to print.")
+        >>> wprint("Anything you want to print.", color="red")
+    """
+    color_list: list[str] = [
+        "grey",
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "magenta",
+        "cyan",
+    ]
+    cprint(
+        text=content,
+        color=choice(color_list) if color not in color_list else color,
+        attrs=["bold"],
+    )
 
 
-def hide():
-    """Hide text (print in grey)"""
-    print(f"\033[0;30;40m", end="")
+class HidePrint:
+    """
+    Hide Print
+    --------
+    Usages:
+    --------
+        >>> with HidePrint():
+        >>>     print("Text that needs to be hidden.")
+    """
 
+    def __enter__(self) -> None:
+        self._original_stdout: TextIO = sys.stdout
+        sys.stdout = open(os.devnull, "w")
 
-def show():
-    """Show text (back in white)"""
-    print(colored("", "white"), end="")
-
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
 
 
 if __name__ == "__main__":
-    # makedir(path='.', name='dir')
     wprint(123, "red")
-    hide()
-    print(1)
-    show()
-    wprint(1)
-    print(__name__)
+    wprint(123)
+    wprint(123, "grey")
+    with HidePrint():
+        print(111)
+    ic(__name__)
